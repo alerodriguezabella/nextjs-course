0c3445e9ced822d75558140056363a4e9422bb00
Ale <alejandra.rodriguez@akqa.com>
2023-07-06 15:53:25 +0200

    Add notifications (through context) to comments and newsletter registration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import Layout from "../components/event-detail/layout/layout";
+import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import Head from "next/head";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
